095684de570886530c6d852c98cc697629fb25a7
Nabetani <takenori@nabetani.sakura.ne.jp>
2022-01-17 21:18:20 +0900

    除算もする

--- go/main.go
+++ go/main.go
@@ -9,10 +9,11 @@ func main(){
 	den:=big.NewInt(6074001001)
 	r := big.NewInt(0)
 	m := big.NewInt(0)
+	seven := big.NewInt(7)
 	for x:=int64(1) ; x<=N ; x++{
 		n0 := big.NewInt(N-x)
 		x0 := big.NewInt(x)
-		z := new(big.Int).Exp(n0, x0, nil)
+		z := new(big.Int).Div(new(big.Int).Exp(n0, x0, nil),seven)
 		d := new(big.Int).Mod(z,den)
 		if 0<d.Cmp(m) {
 			m=d
