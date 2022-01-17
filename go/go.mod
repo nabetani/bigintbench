b0320dcb2cb939f6a81e79ab37cf852a797b8e6b
Nabetani <takenori@nabetani.sakura.ne.jp>
2022-01-17 19:22:03 +0900

    go版を入れる

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/nabetani/bigintbench/go
+
+go 1.17
---
+++ go/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "math/big"
+import "fmt"
+
+const N=10000
+
+func main(){
+	den:=big.NewInt(6074001001)
+	r := big.NewInt(0)
+	m := big.NewInt(0)
+	for x:=int64(1) ; x<=N ; x++{
+		n0 := big.NewInt(N-x)
+		x0 := big.NewInt(x)
+		z := new(big.Int).Exp(n0, x0, nil)
+		d := new(big.Int).Mod(z,den)
+		if 0<d.Cmp(m) {
+			m=d
+			r=x0
+		}
+	}
+	fmt.Println(r)
+}
